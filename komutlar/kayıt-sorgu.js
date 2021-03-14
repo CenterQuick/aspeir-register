9e0ac6f1820f7c6ba4cb161dde25ad5946575922
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-03-14 23:32:09 +0300

    Update kayıt-sorgu.js

--- komutlar/kayıt-sorgu.js
+++ komutlar/kayıt-sorgu.js
@@ -3,7 +3,7 @@ const db = require("quick.db");
 
 module.exports.run = async (client, message, args) => {
 
-    if(!message.member.roles.cache.some(r => ['803679504479420476'].includes(r.id)) && (!message.member.hasPermission("ADMINISTRATOR")))
+    if(!message.member.roles.cache.some(r => ['799666873443680367'].includes(r.id)) && (!message.member.hasPermission("ADMINISTRATOR")))
     return message.reply("Bu Komutu Kullanmak İçin Yetkiniz Bulunmamakta.")
  
  let kullanıcı = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
