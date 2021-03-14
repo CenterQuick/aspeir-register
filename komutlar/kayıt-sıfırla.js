ba8a4663a0afb658afa8c32b126547c0cad1f220
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-03-14 23:32:29 +0300

    Update kayıt-sıfırla.js

--- komutlar/kayıt-sıfırla.js
+++ komutlar/kayıt-sıfırla.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
   
-if(!["803669804765413376"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
+if(!["799666853500682241"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
 
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
