373c752e15dd6954c6900c6fa1bbb65ff4b97302
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-03-14 20:33:00 +0300

    Update kayıt-kadın.js

--- komutlar/kayıt-kadın.js
+++ komutlar/kayıt-kadın.js
@@ -3,19 +3,19 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
   
-if(!["803679504479420476"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
+if(!["799666873443680367"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
   
-const kadin = message.guild.roles.cache.find(r => r.id === "803679501242073089")
-const kayıtsız = message.guild.roles.cache.find(r => r.id === "803679506837143602")
-const reglog = message.guild.channels.cache.find(c => c.id === "803679527526989824")
-const genelchat = message.guild.channels.cache.find(g => g.id === "803679538756190229")
+const kadin = message.guild.roles.cache.find(r => r.id === "799666882986508329")
+const kayıtsız = message.guild.roles.cache.find(r => r.id === "799666885095718913")
+const reglog = message.guild.channels.cache.find(c => c.id === "799569943921688577")
+const genelchat = message.guild.channels.cache.find(g => g.id === "799391453108371530")
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
 if(!member) return message.channel.send(`Bir Kullanıcı Belirt.`)
 if(!member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(`Bu Kullanıcı Sizle Üst/Aynı Pozisyondadır.`)
 const x = message.guild.member(member)
 
-let tag = "ꖜ"
+let tag = "Ψ"
 let isim = args[1]
 let yas = Number(args[2])
 if(!isim) return message.channel.send(`Bir İsim Belirt`)
@@ -51,7 +51,7 @@ const embed = new Discord.MessageEmbed()
     .setThumbnail(message.author.avatarURL())
 .setFooter(`Heartling Register`)
 .setColor('GREEN')
-client.channels.cache.get('803679538756190229').send(embed)
+client.channels.cache.get('799391453108371530').send(embed)
 
 
 genelchat.send(`<@${member.id}>, Aramıza Hoş Geldin ! Umarım Keyifli Vakitler Geçirirsin.`)
