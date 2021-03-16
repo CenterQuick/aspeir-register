74826b4f531373b36f074b71e0cfb84433630c27
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-03-16 11:28:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,7 +218,20 @@ client.on("guildMemberAdd", member => {
   }
 });
 
-//-----------------------TAG-KONTROL----------------------\\
+//-----------------------HG-MESAJI----------------------\\
+
+client.on("guildMemberAdd", member => {
+  const hgembed = new Discord.MessageEmbed()
+    .setAuthor(`${client.user.username} Yardım`,client.user.avatarURL())
+    .setColor("RED")
+    .addField(`<@${member.id}> İsimli şahıs sunucuya Katıldı`)
+    .addField(`Kayıt olmak için @Ψ Teyit Görevlisi ile iletişime geçebilirsin.`)
+    .addField(`Tagımızı alarak bize destek olabilirsin ; Ψ`)
+    .setFooter(`${message.author.username}`, message.author.avatarURL)
+    .setTimestamp();
+  client.channels.cache.get("799569943921688577").send(hgembed);
+  }
+});
 
 //-----------------------İSİM-DEĞİŞTİRME----------------------\\
 
